2ee9eb624d44420393371f95b489e74efbdd7d40
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-14 21:01:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,12 +20,6 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}"
   end 
   
-  get '/multiply/:num1/:num2' do 
-    num1 = params[:num1]
-    num2 = params[:num2]
-    product = num1*num2
-    
-    "product"
-  do 
+  
 
 end
